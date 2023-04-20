b9d3c07a3e989ccdb9f77d69d2f7102ba58aca38
AiSpark <122905588+AiSpark1@users.noreply.github.com>
2023-04-20 18:12:46 +0000

    upd

--- clickserver/app.py
+++ clickserver/app.py
@@ -12,7 +12,7 @@ def get_path(path):
 
 
 leave_png = cv2.imread(get_path('leave_blue.png'))
-white_robo = cv2.imread(get_path('white_robo_tr.png'))
+white_robo = cv2.imread(get_path('white_robo.png'))
 reload_png = cv2.imread(get_path('reload.png'))
 
 
